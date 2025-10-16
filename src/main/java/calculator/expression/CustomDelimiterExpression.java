34a42ac9c745476b94793a7d9ea38d457c30b9b0
chxghee <francis001021@gmail.com>
2025-10-16 13:38:07 +0900

    feat: 커스텀 구분자를 사용하는 CustomDelimiterExpression 구현체 추가
    - 기본 구분자(, :)와 커스텀 구분자를 사용하는 수식을 표현하는 클래스 추가
    - Expression 인터페이스 구현

---
+++ src/main/java/calculator/expression/CustomDelimiterExpression.java
@@ -0,0 +1,42 @@
+package calculator.expression;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Pattern;
+
+public class CustomDelimiterExpression implements Expression {
+
+    private static final List<String> BASIC_DELIMITERS = List.of(",", ":");
+    private final String expressionBody;
+    private final String customDelimiter;
+
+    private CustomDelimiterExpression(String expressionBody, String customDelimiter) {
+        this.expressionBody = expressionBody;
+        this.customDelimiter = customDelimiter;
+    }
+
+    public static CustomDelimiterExpression of(String inputString, String prefix, String suffix) {
+        int delimiterEndIndex = inputString.indexOf(suffix);
+
+        if (delimiterEndIndex < prefix.length()) {
+            throw new IllegalArgumentException("잘못된 커스텀 구분자 입력 형식입니다.");
+        }
+
+        String customDelimiter = inputString.substring(prefix.length(), delimiterEndIndex);
+        String expressionBody = inputString.substring(delimiterEndIndex + suffix.length());
+
+        return new CustomDelimiterExpression(expressionBody, customDelimiter);
+    }
+
+    @Override
+    public String getExpressionBody() {
+        return this.expressionBody;
+    }
+
+    @Override
+    public List<String> getDelimiters() {
+        ArrayList<String> delimiters = new ArrayList<>(BASIC_DELIMITERS);
+        delimiters.add(Pattern.quote(this.customDelimiter));
+        return delimiters;
+    }
+}
