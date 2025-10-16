7ba1963e39ee8fdcf9ff32852207e223fe0897bc
chxghee <francis001021@gmail.com>
2025-10-16 14:12:52 +0900

    feat: 문자열 덧셈 결과 입출력 구현

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,14 @@
 package calculator;
 
+import calculator.calculation.StringCalculator;
+
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        String input = readLine();
+        int result = StringCalculator.add(input);
+        System.out.println("결과 : " +result);
     }
 }
